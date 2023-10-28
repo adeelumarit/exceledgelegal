7d76e3def1b3a419e7fc62c79537f8075fc49397
adeelumar-it <adeelumar154@gmail.com>
2023-10-28 09:18:21 +0500

    edge testing

---
+++ ExcelFileConverterWeb/Controllers/HomeController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WordWebLanguageTranslaterWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+
+        public class ContactRequest
+        {
+            public string ContactDetail { get; set; }
+            public string token { get; set; }
+        }
+       
+        public class LoginRequest
+        {
+            public string userName { get; set; }
+            public string password { get; set; }
+        }
+        [HttpPost]
+        public async Task<string> Login(LoginRequest loginRequest)
+        {
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Post, "https://edgelegal.co/LPDM/RT/WS/login");
+
+                // Create a FormData and add the login request parameters
+                var formData = new MultipartFormDataContent();
+                formData.Add(new StringContent(loginRequest.userName), "userName");
+                formData.Add(new StringContent(loginRequest.password), "password");
+
+                // Set the content of the request to the FormData
+                request.Content = formData;
+
+                // Send the request
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+                }
+                else
+                {
+                    string errorResponse = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"HTTP error status code: {response.StatusCode}");
+                    return errorResponse;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur during the request
+                Console.WriteLine($"Error: {ex.Message}");
+                return $"Error: {ex.Message}"; // Return the error message as a string
+            }
+        }
+
+
+        public class FileUploadRequest
+        {
+            public Stream FileStream { get; set; }
+            public string FileName { get; set; }
+            public string ContentDispositionHeader { get; set; }
+            public string OriginalName { get; set; }
+            public string MatterNumber { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<string> UploadMatterAttachment(FileUploadRequest fileUploadRequest)
+        {
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Post, "https://edgelegal.co/LPDM/RT/WS/uploadMatterAttachment");
+
+                // Create a FormData and add the file and other parameters
+                var formData = new MultipartFormDataContent();
+                formData.Add(new StreamContent(fileUploadRequest.FileStream), "fileInputStream", fileUploadRequest.FileName);
+                formData.Add(new StringContent(fileUploadRequest.ContentDispositionHeader), "contentDispositionHeader");
+                formData.Add(new StringContent(fileUploadRequest.OriginalName), "originalName");
+                formData.Add(new StringContent(fileUploadRequest.MatterNumber.ToString()), "matterNumber");
+
+                // Set the content of the request to the FormData
+                request.Content = formData;
+
+                // Send the request
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+                }
+                else
+                {
+                    string errorResponse = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"HTTP error status code: {response.StatusCode}");
+                    return errorResponse;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur during the request
+                Console.WriteLine($"Error: {ex.Message}");
+                return $"Error: {ex.Message}"; // Return the error message as a string
+            }
+        }
+
+        //public async Task<string> createContact(ContactRequest contactRequest)
+        //{
+        //    try
+        //    {
+        //        var client = new HttpClient();
+        //        var request = new HttpRequestMessage(HttpMethod.Post, "https://api.hubapi.com/crm/v3/objects/contacts");
+        //        request.Headers.Add("Authorization", "Bearer " + contactRequest.token);
+        //        var content = new StringContent(contactRequest.ContactDetail, null, "application/json");
+        //        request.Content = content;
+        //        var response = await client.SendAsync(request);
+        //        response.EnsureSuccessStatusCode();
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            string responseBody = await response.Content.ReadAsStringAsync();
+        //            return responseBody;
+        //        }
+        //        else
+        //        {
+        //            string errorResponse = await response.Content.ReadAsStringAsync();
+        //            Console.WriteLine($"HTTP error status code: {response.StatusCode}");
+        //            return errorResponse;
+        //        }
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Handle any exceptions that occur during the request
+        //        Console.WriteLine($"Error: {ex.Message}");
+        //        return $"Error: {ex.Message}"; // Return the error message as a string
+        //    }
+
+        //}
+
+    }
+}
---
+++ ExcelFileConverterWeb/Home.js
@@ -0,0 +1,518 @@
+﻿// <reference path="messageread.js" />
+
+
+
+var app = angular.module('edgelegal', ['ngMaterial', "ngRoute"], function () {
+
+
+});
+
+
+app.controller('edgelegalctrl', function ($scope, $mdDialog, $mdToast, $log, $location,) {
+    var baseURL = "https://grazingdelights.com.au/LPDM/RT/WS";
+    var dialogueURL ="https://localhost:44311"
+    $scope.ShowMainDiv = false;
+    $scope.wordMainDiv = true;
+    $scope.loginBTN = false;
+
+    var filecontent = "";
+    let MatterNumberdialog;
+    let logdialog;
+    Office.onReady(function (info) {
+
+    ProgressLinearActive();
+
+
+    let userInfo = window.localStorage.getItem('userinfo');
+    userInfo = JSON.parse(userInfo)
+        let username = "";
+
+        if (userInfo) {
+            $scope.ShowMainDiv = false;
+            $scope.userName = userInfo.username;
+            
+
+            if (info.host === Office.HostType.Excel) {
+
+                $scope.ShowMainDiv = true;
+                $scope.wordMainDiv = true
+                ProgressLinearInActive();
+
+                //loadtost("excel addin is wroking ")
+
+
+
+
+            } else {
+                $scope.ShowMainDiv = false;
+                $scope.wordMainDiv = true;
+
+                ProgressLinearInActive();
+
+                loadtost("word addin is wroking ")
+                loadtost("no functioanlities included yet")
+
+            }
+
+            ProgressLinearInActive();
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+
+
+        } else {
+            
+            $scope.ShowMainDiv = false;
+            $scope.wordMainDiv = true;
+            $scope.loginBTN = true;
+            ProgressLinearInActive();
+            openDialog();
+
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        }
+
+  
+
+        $scope.Login = function () {
+            openDialog();
+        }
+    function openDialog() {
+
+      
+        Office.context.ui.displayDialogAsync(dialogueURL+`/Templates/Login.html`, { height: 50, width: 30 },
+            function (asyncResult) {
+                logdialog = asyncResult.value;
+                logdialog.addEventHandler(Office.EventType.DialogMessageReceived, logprocess);
+            }
+        );
+    }
+       
+
+
+
+        function logprocess(arg) {
+            ProgressLinearActive();
+
+        logdialog.close();
+        console.log(arg)
+        let message = JSON.parse(arg.message);
+        //let userdata = message;
+        //message = message.login;
+        if (message.login === true) {
+            console.log(message)
+            window.localStorage.setItem('userinfo', JSON.stringify(message));
+
+            //$scope.userName = userdata.userName
+            loadToast("logged in successfully")
+
+
+
+            //$scope.ShowMainDiv = true;
+            //$scope.loginBTN = false;
+            window.location.reload();
+            ProgressLinearInActive();
+
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+            //$scope.Message = false;
+        } else {
+            //dialog.close();
+            $scope.ShowMainDiv = false;
+            $scope.loginBTN = true;
+            ProgressLinearInActive();
+            //loadToast("Refresh Addin To Login ")
+
+            //$scope.Message = true;
+        }
+    }
+
+       
+        function getDocumentAsCompressed() {
+            Office.context.document.getFileAsync(Office.FileType.Compressed, { sliceSize: 4194304  /*64 KB*/ }, function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    const myFile = result.value;
+                    const sliceCount = myFile.sliceCount;
+                    const docdataSlices = [];
+                    let slicesReceived = 0;
+                    let gotAllSlices = true;
+
+                    // Get all the file slices.
+                    getSliceAsync(myFile, 0, sliceCount, gotAllSlices, docdataSlices, slicesReceived);
+                } else {
+                    console.log("Error: " + result.error.message);
+                }
+            });
+        }
+
+        function getSliceAsync(file, nextSlice, sliceCount, gotAllSlices, docdataSlices, slicesReceived) {
+            file.getSliceAsync(nextSlice, function (sliceResult) {
+                if (sliceResult.status === Office.AsyncResultStatus.Succeeded) {
+                    if (!gotAllSlices) {
+                        return; // Failed to get all slices, no need to continue.
+                    }
+
+                    // Store the slice in a temporary array.
+                    docdataSlices[sliceResult.value.index] = sliceResult.value.data;
+                    slicesReceived++;
+
+                    if (slicesReceived === sliceCount) {
+                        file.closeAsync(function () {
+                            onGotAllSlices(docdataSlices);
+                        });
+                    } else {
+                        getSliceAsync(file, nextSlice + 1, sliceCount, gotAllSlices, docdataSlices, slicesReceived);
+                    }
+                } else {
+                    gotAllSlices = false;
+                    file.closeAsync(function () {
+                        console.log("getSliceAsync Error: " + sliceResult.error.message);
+                    });
+                }
+            });
+        }
+
+        function onGotAllSlices(docdataSlices) {
+            let docdata = new Uint8Array(docdataSlices.reduce((acc, slice) => acc.concat(slice), []));
+
+            const blob = new Blob([docdata], { type: "application/octet-stream" });
+            const reader = new FileReader();
+
+            reader.onloadend = function () {
+                const base64Data = reader.result.split(',')[1];
+                console.log(base64Data);
+                if (base64Data) {
+                    //file.closeAsync();
+                    filecontent = base64Data;
+                    Excel.run(function (context) {
+                        var workbook = context.workbook;
+
+                        workbook.load(["name"]);
+
+                        return context.sync()
+                            .then(function () {
+                                // Access the workbook name
+                                var workbookName = workbook.name;
+                                $scope.Filename = workbookName;
+                                Office.context.ui.displayDialogAsync(dialogueURL +`/Templates/MatterNumber.html?workbookName=${workbookName}`, { height: 50, width: 30 },
+                                    function (asyncResult) {
+                                        MatterNumberdialog = asyncResult.value;
+                                        MatterNumberdialog.addEventHandler(Office.EventType.DialogMessageReceived, processMessage);
+                                    }
+                                );
+
+                                //ProgressLinearInActive();
+                                console.log("Active workbook name: " + workbookName);
+                            })
+                            .catch(function (error) {
+                                console.log("Error: " + error);
+                            });
+                    }).catch(function (error) {
+                        console.log("Error: " + error);
+                    });
+                }
+                // Now you have the complete file data in base64 format.
+                // You can use it as needed.
+            };
+
+            reader.readAsDataURL(blob);
+        }
+
+
+
+        //getDocumentAsCompressed();
+
+        $scope.getFilebase64 = function (pdfvalue) {
+            console.log(pdfvalue)
+            getDocumentAsCompressed(pdfvalue);
+
+
+
+        };
+
+
+        $scope.GetFileAsPDF = function () {
+
+            loadToast("working on pdf")
+        }
+           //return new Promise(function (resolve, reject) {
+            //    Office.context.document.getFileAsync(Office.FileType.Compressed, { sliceSize: 4194304 }, function (result) {
+            //        try {
+            //            if (result.status === Office.AsyncResultStatus.Succeeded) {
+            //                var file = result.value;
+            //                var base64Data = "";
+            //                var offset = 0;
+
+            //                function readNextChunk() {
+            //                    file.getSliceAsync(offset, function (sliceResult) {
+            //                        if (sliceResult.status === Office.AsyncResultStatus.Succeeded) {
+            //                            var dataSlice = sliceResult.value.data;
+            //                            var reader = new FileReader();
+            //                            reader.onloadend = function () {
+            //                                base64Data += reader.result.split(',')[1];
+            //                                offset += dataSlice.byteLength;
+            //                                if (offset < file.size) {
+            //                                    readNextChunk();
+            //                                    file.closeAsync();
+
+            //                                } else {
+
+
+
+            //                                    resolve(base64Data);
+
+
+
+
+            //                                    console.log(base64Data)
+            //                                }
+            //                            };
+            //                            reader.readAsDataURL(new Blob([new Uint8Array(dataSlice)]));
+
+            //                            file.closeAsync()
+            //                        } else {
+            //                            file.closeAsync()
+
+            //                            reject(sliceResult.error);
+            //                        }
+            //                    });
+            //                }
+
+            //                readNextChunk();
+            //            } else {
+            //                reject(result.error);
+            //                if (result.error.code === 5001) {
+
+            //                    loadToast("Limit reached, try again to Reopen your document");
+            //                }
+            //            }
+            //        } catch (error) {
+            //            console.log(error);
+            //            reject(error);
+            //        }
+            //    });
+            //});
+
+        function processMessage(arg) {
+            ProgressLinearActive();
+
+            MatterNumberdialog.close();
+
+            console.log(arg)
+            let message = JSON.parse(arg.message);
+            if (message.close == true) {
+
+                MatterNumberdialog.close();
+                ProgressLinearInActive();
+
+            } else {
+                console.log(message)
+                message.filecontent = filecontent;
+                console.log(message);
+                var form = new FormData();
+                form.append("originalName", message.originalName);
+                form.append("matterId", message.matternumber);
+                form.append("userName", $scope.userName);
+                form.append("", filecontent);
+                //form.append("UserName", "Aamir");
+
+                var settings = {
+                    "url": baseURL+"/uploadMatterAttachmentV2",
+                    "method": "POST",
+                    "timeout": 0,
+                    "headers": {
+                        //"Cookie": username
+                    },
+                    "processData": false,
+                    "mimeType": "multipart/form-data",
+                    "contentType": false,
+                    "data": form
+                };
+
+                $.ajax(settings).done(function (response) {
+                    console.log(response);
+                    loadToast("Uploaded Successfuly");
+                    ProgressLinearInActive();
+                }).fail(function (error) {
+
+                    console.log(error);
+                    loadToast("upload error");
+                    ProgressLinearInActive();
+                });
+            }
+        }
+
+        //convertToPDF();
+
+        //cc = function () {
+        //    return new Promise(function (resolve, reject) {
+        //        Office.context.document.getFileAsync(Office.FileType.Compressed, { sliceSize: 4194304 }, function (result) {
+        //            try {
+        //                if (result.status === Office.AsyncResultStatus.Succeeded) {
+        //                    var file = result.value;
+        //                    file.getSliceAsync(0, function (sliceResult) {
+        //                        try {
+        //                            if (sliceResult.status === Office.AsyncResultStatus.Succeeded) {
+        //                                var dataSlice = sliceResult.value.data;
+        //                                console.log(dataSlice)
+        //                                var base64Data = btoa(String.fromCharCode.apply(null, new Uint8Array(dataSlice)));
+        //                                console.log(base64Data);
+        //                                resolve(base64Data);
+        //                                $scope.CompilingDocumentasPDF(base64Data);
+        //                                file.closeAsync()
+
+        //                                // convertToPdfUsingAjax(base64Data);
+        //                            } else {
+        //                                file.closeAsync()
+
+        //                                reject(sliceResult.error);
+
+        //                            }
+        //                        } catch (error) {
+        //                                file.closeAsync()
+
+        //                            if (error instanceof RangeError && error.message === "Maximum call stack size exceeded") {
+        //                                loadToast("Opened Document size limit reached");
+        //                            } else {
+        //                                reject(error);
+        //                            }
+        //                        }
+        //                    });
+        //                } else {
+        //                    reject(result.error);
+        //                    if (result.error.code === 5001) {
+        //                        loadToast("PDF Limit reached, try again to Reopen your document");
+        //                    }
+        //                }
+        //            } catch (error) {
+        //                reject(error);
+        //            }
+        //        });
+        //    });
+        //};
+        //$scope.getFilebase64 = function (ev) {
+
+        //    Office.context.document.getFileAsync(Office.FileType.Compressed, { sliceSize: 4194304},
+        //        function (result) {
+        //            if (result.status === "succeeded") {
+        //                const myFile = result.value;
+        //                myFile.getSliceAsync(0, function (result) {
+
+        //                    let data = result.value.data
+        //                    console.log(btoadata)
+
+
+        //                    let btoadata = btoa(String.fromCharCode.apply(null, new Uint8Array(data)))
+
+        //                    console.log(btoadata)
+
+        //                    if (btoadata !="") {
+        //                        myFile.closeAsync();
+        //                        filecontent = btoadata;
+        //                        Excel.run(function (context) {
+        //                            var workbook = context.workbook;
+
+        //                            workbook.load(["name"]);
+
+        //                            return context.sync()
+        //                                .then(function () {
+        //                                    // Access the workbook name
+        //                                    var workbookName = workbook.name;
+        //                                    $scope.Filename = workbookName;
+        //                                    Office.context.ui.displayDialogAsync(`https://localhost:44311/Templates/MatterNumber.html?workbookName=${workbookName}`, { height: 50, width: 30 },
+        //                                        function (asyncResult) {
+        //                                            MatterNumberdialog = asyncResult.value;
+        //                                            MatterNumberdialog.addEventHandler(Office.EventType.DialogMessageReceived, processMessage);
+        //                                        }
+        //                                    );
+
+        //                                    //ProgressLinearInActive();
+        //                                    console.log("Active workbook name: " + workbookName);
+        //                                })
+        //                                .catch(function (error) {
+        //                                    console.log("Error: " + error);
+        //                                });
+        //                        }).catch(function (error) {
+        //                            console.log("Error: " + error);
+        //                        });
+        //                    }
+
+
+
+        //                });
+
+        //            } else {
+        //                myFile.closeAsync()
+
+        //                // Handle the error here
+        //                //loadToast("Upload Error");
+        //                //ProgressLinearInActive();
+
+        //            }
+
+        //        }
+        //    );
+        //}
+
+
+
+    });
+    $scope.Logout = function () {
+        window.localStorage.clear();
+        window.location.reload();
+    }
+
+    $scope.Help = function () {
+        window.open("https://support.microsoft.com/en-us")
+    }
+
+        function ProgressLinearActive() {
+            $("#StartProgressLinear").show(function () {
+
+                $("#ProgressBgDiv").show();
+                $scope.ddeterminateValue = 15;
+                $scope.showProgressLinear = false;
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
+            });
+        };
+        function ProgressLinearInActive() {
+            $("#StartProgressLinear").hide(function () {
+                setTimeout(function () {
+                    $scope.ddeterminateValue = 0;
+                    $scope.showProgressLinear = true;
+                    $("#ProgressBgDiv").hide();
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                }, 500);
+            });
+        };
+        function loadToast(alertMessage) {
+            var el = document.querySelectorAll('#zoom');
+            $mdToast.show(
+                $mdToast.simple()
+                    .textContent(alertMessage)
+                    .position('bottom')
+                    .hideDelay(4000))
+                .then(function () {
+                    $log.log('Toast dismissed.');
+                }).catch(function () {
+                    $log.log('Toast failed or was forced to close early by another toast.');
+                });
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        };
+
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+
+  
+      
+   
+})
